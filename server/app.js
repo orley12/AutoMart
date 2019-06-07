7f4851f76b7a81332eee908389506d2d79aa4cda
orley <solarintosin@yahoo.com>
2019-06-07 20:25:32 +0100

    deploy to heroku 2

--- server/app.js
+++ server/app.js
@@ -11,25 +11,28 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/car', carRoutes);
-
-
-app.use((req, res, next) => {
-  console.log('CALLED JOB');
-  const err = new Error('Resource Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', (req, res, next) => {
+  res.send('hello world');
 });
+// app.use('/api/v1/auth', authRoutes);
+// app.use('/api/v1/car', carRoutes);
 
-app.use((err, req, res, next) => {
-  console.log('CALLED JOB');
-  res.json({
-    status: err.status || 500,
-    message: err.message,
-    errors: err.errors,
-  });
-});
+
+// app.use((req, res, next) => {
+//   console.log('CALLED JOB');
+//   const err = new Error('Resource Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// app.use((err, req, res, next) => {
+//   console.log('CALLED JOB');
+//   res.json({
+//     status: err.status || 500,
+//     message: err.message,
+//     errors: err.errors,
+//   });
+// });
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 // eslint-disable-next-line no-console
