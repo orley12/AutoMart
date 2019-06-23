da3ebbe167fcbed9e6f26e9c5ed68e0ec9dd4bf7
orley <solarintosin@yahoo.com>
2019-06-23 20:06:03 +0100

    chore(database) setup travis ci database
    [Finishes #166861237]

--- server/model/seed.js
+++ server/model/seed.js
@@ -1,22 +1,23 @@
 /* eslint-disable consistent-return */
-import authRepository from '../repository/authRepository';
+import authUtil from '../util/authUtil';
 import db from './db';
 
-const seedTable = () => {
+const seedTable = async () => {
   try {
-    const queryUser = db.query(`
-  INSERT INTO users ("firstname", "lastname", email, password, address, "isadmin") 
-  VALUES ('john', 'doe', 'johndoe@gmail.com', '${authRepository.hashPassword('manjen102')}', '75 Bode Thomas Surulere', false),
-  ('manny', 'jenson', 'mannyjenson@gmail.com', '${authRepository.hashPassword('manjen102')}', '75 Bode Thomas Surulere', false),
-  ('sam', 'peterson', 'sam@gmail.com', '${authRepository.hashPassword('manjen102')}', '75 Bode Thomas Surulere', false); `);
+    const queryUser = await db.query(`
+  INSERT INTO users (firstName, lastName, email, password, phone, address) 
+  VALUES ('john', 'doe', 'johndoe@gmail.com', '${authUtil.hashPassWord('password')}', '090888777665', '75 Bode Thomas Surulere'),
+  ('manny', 'jenson', 'mannyjenson@gmail.com', '${authUtil.hashPassWord('password')}', '090888777665', '75 Bode Thomas Surulere'),
+  ('sam', 'peterson', 'sam@gmail.com', '${authUtil.hashPassWord('password')}','090888777665', '75 Bode Thomas Surulere'); `);
 
-    // const queryLoan = await db.query(`
-    //   INSERT INTO loans ("useremail", createdon, status, repaid, tenor, amount, paymentInstallment, balance, interest)
-    //     VALUES ('doniyke44@gmail.com', '${moment(new Date())}', 'approved', 'false', 12, 200000, 26666.52, 200000, 2500),
-    //            ('peperenpe@gmail.com', '${moment(new Date())}', 'approved', 'true', 12, 200000, 26666.52, 0.00, 2500);`);
+    const queryCar = await db.query(`
+      INSERT INTO cars (state, price, manufacturer, model,
+        bodyType, transmission, milage, year, exteriorImg, ownerEmail, owner)
+        VALUES ('new', '750000', 'Benz', 'SLS', 'coupe', 'automatic', '5000', '2020', 'https://res.cloudinary.com/automart-api/image/upload/v1561297529/exterior/HHHNT6zcV3E1_zZezHKf4gDH.png', 'sam@gmail.com', 4),
+        ('used', '750000', 'Audi', 'maybach', 'coupe', 'manual', '5000', '1889', 'https://res.cloudinary.com/automart-api/image/upload/v1561297529/exterior/HHHNT6zcV3E1_zZezHKf4gDH.png', 'mannyjenson@gmail.com', 3);`);
 
 
-    console.log(queryUser /* , queryLoan */);
+    console.log(queryUser, queryCar);
   } catch (err) {
     // eslint-disable-next-line no-console
     console.log(err.stack);
