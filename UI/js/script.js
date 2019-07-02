d0b8f4b021461a10d114b8a2ff1e5be0d2141348
orley <solarintosin@yahoo.com>
2019-07-02 16:02:07 +0100

    feature(signin fullapp): connect fornt and back-end dor signin
    [Finishes #166998669]

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,12 +1,12 @@
-const openNavBtn = document.querySelector("#open-nav");
-const closeNavBtn = document.querySelector("#close-nav");
+const openNavBtn = document.querySelector('#open-nav');
+const closeNavBtn = document.querySelector('#close-nav');
 
-openNavBtn.addEventListener("click", () => {
-  document.getElementById("side-menu").style.width = "250px";
+openNavBtn.addEventListener('click', () => {
+  document.getElementById('side-menu').style.width = '250px';
   // document.getElementById("main").style.marginLeft = "250px";
 });
 
-closeNavBtn.addEventListener("click", () => {
-  document.getElementById("side-menu").style.width = "0";
+closeNavBtn.addEventListener('click', () => {
+  document.getElementById('side-menu').style.width = '0';
   // document.getElementById("main").style.marginLeft = "250px";
 });
